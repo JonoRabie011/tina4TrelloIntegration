e0e436cba9c5d3f52135e61959a7cb8d09d7f962
Jonathan <jonathan@ksena.co.za>
2023-05-29 23:36:09 +0200

    Patch fix for Integration

--- loadModule.php
+++ loadModule.php
@@ -1,5 +1,5 @@
 <?php
 
-\Tina4\Module::addModule("Tina4 Trello Integration", "1.0.0", "Tina4TrelloIntegration", function(\Tina4\Config $config) {
+\Tina4\Module::addModule("Tina4 Trello Integration", "0.0.4", "Tina4TrelloIntegration", function(\Tina4\Config $config) {
     //(new Content())->addConfigMethods($config);
 });
